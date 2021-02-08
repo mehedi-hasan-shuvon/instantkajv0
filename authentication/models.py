b629264a4f6bd080c3afe10a447f5d840bba9042
Jamiul Bari <jamiul.bari98@gmail.com>
2021-02-09 02:42:02 +0600

    Login added

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=20, default="FirstName")
+    last_name = models.CharField(max_length=20, default="LastName")
+    email = models.EmailField()
+    password = models.CharField(max_length=12)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} -- {self.email}'
