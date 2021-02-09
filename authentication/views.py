3a689db8a498fd7cb547689605951c9b51ed7f75
Jamiul Bari <jamiul.bari98@gmail.com>
2021-02-09 23:17:40 +0600

    Added User Registration ~ has ModelForm existence conflict

--- authentication/views.py
+++ authentication/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 
 from .models import User
-from .forms import LoginForm
+from .forms import LoginForm, RegisterForm, RegisterFormModel
 
 # Create your views here.
 def index(request):
@@ -15,7 +15,7 @@ def index(request):
             #  check w/ data
             email = form.cleaned_data['email']
             password = form.cleaned_data['password']
-            exists = User.objects.filter(email=email, password=password)
+            exists = User.objects.filter(email=email, password=password).exists()
             if exists:
                 return HttpResponseRedirect(reverse('success'))
             else:
@@ -34,4 +34,70 @@ def failure(request):
     return render(request, 'authentication/failure.html')
 
 def register(request):
-    return render(request, 'authentication/register.html')
+
+    form = RegisterForm()
+    message = ""
+    if request.method == "POST":
+        # user sent data for registration
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            # check data for duplicate
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+
+            exists = User.objects.filter(email=email, password=password).exists()
+
+            if not exists:
+                User.objects.create(first_name=first_name, last_name=last_name, email=email, password=password)
+                message = "Account Created"
+
+                # Reset Form
+                form = RegisterForm()
+            else:
+                message = f"An account already exists with {email}"
+        else:
+            message = "Form not valid"
+    else:
+        form = RegisterForm()
+        message = "Complete the form to register"
+
+    
+    return render(request, 'authentication/register.html', {'form': form, 'message': message})
+
+# Bug exists
+def register2(request):
+
+    form = RegisterFormModel()
+    message = ""
+    if request.method == "POST":
+        form = RegisterFormModel(request.POST)
+
+        if form.is_valid():
+            print("form valid")
+            # check data for duplicate
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+
+            exists = User.objects.filter(email=email, password=password).exists()
+
+            if not exists:
+                form.save()
+                message = "Account Created"
+
+                # Reset Form
+                form = RegisterForm()
+            else:
+                message = f"An account already exists with {email}"
+        else:
+            print("form not valid")
+            message = "Form not valid"
+    else:
+        form = RegisterFormModel()
+        message = "Complete the form to register"
+
+    return render(request, 'authentication/register2.html', {'form': form, 'message': message})
